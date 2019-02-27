98e434bf9e1ac02330159068fa34ab934b74921d
Surya C S <48043203+cssurya@users.noreply.github.com>
2019-02-27 11:13:50 +0530

    Update and rename Number.java to vowel.java

---
+++ vowel.java
@@ -0,0 +1,15 @@
+public class positive{
+public static void main(String[]args){
+  char c;
+Scanner sc=new Scanner(System.in);
+  c=sc.next();
+if(c.equals('a'|'e'|'i'|'o'|'u'))
+{
+System.out.println("vowel");
+}
+else
+{
+System.out.println("constant");
+}
+}
+}
